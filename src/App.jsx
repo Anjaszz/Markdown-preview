b06ff3ea717725ce5f1ec1bedbc06891628a35fa
Anjaszz <anjasgans841@gmail.com>
2024-09-16 11:25:39 +0700

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from 'react';
+import './App.css';
+import { marked } from 'marked';
 function App() {
-  const [count, setCount] = useState(0)
+  const [markdown, setMarkdown] = useState('');
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      <h1 className='title'>Markdown Preview</h1>
+      <textarea
+        className="mark"
+        placeholder='Tulis markdown disini'
+        value={markdown}
+        onChange={(e) => setMarkdown(e.target.value)}
+      />
+
+      <div 
+      className="preview"
+      dangerouslySetInnerHTML={{__html: marked(markdown)}}
+      />
+       
+    </div>
+  );
 }
 
-export default App
+export default App;
